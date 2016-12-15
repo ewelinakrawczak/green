34589a1697a6f3c2ec5c8fd7375c9ed037a5553e
Eve <krafffczak@gmail.com>
2016-12-15 23:17:15 +0100

    sass: added after/before mixin ,header animation after onload added to main.js , footer setting

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
-
- $.stellar();
+(function(){
+    
+    var headerH1=$('h1');
+    var headerH2=$('h2');
+    var arrow=$('.arrow');
+    var nav=$('nav>a');
+    
+   $.stellar();
+   
+   $(document).ready(function(){
+       console.log('ready');
+       headerH1.css({
+           '-webkit-animation':'header .75s linear .75s forwards',
+            'animation':'header .75s linear .75s forwards'
+       });
+       headerH2.css({
+           '-webkit-animation':'header .75s linear .75s forwards',
+            'animation':'header .75s linear .75s forwards' 
+       });
+       arrow.css({
+           '-webkit-animation':'header .75s linear 2s forwards',
+            'animation':'header .75s linear 2s forwards' 
+       });
+       nav.css({
+           '-webkit-animation':'anav .75s linear 1.75s forwards',
+            'animation':'anav .75s linear 1.75s forwards' 
+       });
+   });
+   
+}());
+ 
             
             
             
